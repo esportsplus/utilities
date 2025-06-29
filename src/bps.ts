d75a6a671be069153bcc2055f326eb5cefd2bf32
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-06-29 00:16:38 -0700

    refactor: bps helper

--- src/bps.ts
+++ src/bps.ts
@@ -1,9 +1,30 @@
-export default (amount: bigint | number | string, bps: number, max?: bigint | number | string) => {
-    amount = Math.ceil( (Number(amount) * bps ) / 10_000 );
+const PRECISION_FACTOR = 1_000_000_000;
 
-    if (max) {
-        amount = Math.min(Number(max), amount);
+
+function bps(amount: bigint, bps: bigint | number | string): bigint;
+function bps(amount: number | string, bps: bigint | number | string): number;
+function bps(amount: bigint | number | string, bps: bigint | number | string): bigint | number {
+    bps = BigInt(bps);
+
+    if (bps === 0n) {
+        return typeof amount === 'bigint' ? 0n : 0;
     }
 
-    return amount;
+    if (typeof amount === 'bigint') {
+        return amount * bps / 10_000n;
+    }
+
+    amount = Number(amount);
+
+    if (amount === 0) {
+        return 0;
+    }
+
+    amount = Math.round(amount * PRECISION_FACTOR);
+    amount = BigInt(amount) * bps / 10_000n;
+
+    return Number(amount) / PRECISION_FACTOR;
 };
+
+
+export default bps;
