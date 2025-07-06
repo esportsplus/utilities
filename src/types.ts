56c16d37145eedc085f498248adf35389c49fbb7
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-06 01:47:45 -0700

    feat: export utilities types from @esportsplus/typescript

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,45 @@
 type BIPS = number;
 
+type Brand<T, B extends string> = T & { __brand: B };
 
-export type { BIPS };
+type DeepReadonly<T> = T extends (...args: any[]) => any
+    ? T
+    : T extends Array<infer R>
+        ? ReadonlyArray<DeepReadonly<R>>
+        : T extends object
+            ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+            : T;
+
+type Function = (...args: unknown[]) => Promise<unknown> | unknown;
+
+type NeverAsync<T> =
+    T extends Promise<unknown>
+        ? never
+        : T extends (...args: unknown[]) => unknown
+            ? NeverAsync<ReturnType<T>> extends never
+                ? never
+                : T
+            : T;
+
+type NeverFunction<T> =
+    T extends Promise<unknown>
+        ? never
+        : T extends (...args: unknown[]) => unknown
+            ? never
+            : T;
+
+type Prettify<T> = {
+    [K in keyof T]: T[K];
+} & {};
+
+type UnionRecord<U, V extends Record<string, unknown>> = Prettify<U & { [K in keyof U]?: undefined } & V>;
+
+
+export type {
+    BIPS, Brand,
+    DeepReadonly,
+    Function,
+    NeverAsync, NeverFunction,
+    Prettify,
+    UnionRecord
+};
