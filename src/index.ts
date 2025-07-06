eb5d786b53a35796908fc5e11f4de5ee0c91cfa2
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-06 04:18:08 -0700

    fix: missing types

--- src/index.ts
+++ src/index.ts
@@ -31,4 +31,3 @@ export { default as request } from './request';
 export { default as sleep } from './sleep';
 export { default as truncate } from './truncate';
 export type * from './types';
-export type * from '@esportsplus/typescript';
