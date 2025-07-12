7e737eecb3965585540edb26bea19c267e343f68
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-12 04:24:25 -0700

    feat: add search init option for query param conversion

--- src/request.ts
+++ src/request.ts
@@ -2,7 +2,8 @@ import { isObject } from '.';
 
 
 type Init = Omit<RequestInit, 'body' | 'method'> & {
-    method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+    method?: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE',
+    search?: Record<string, string>,
 } & (
     {
         body?: RequestInit['body'] | Record<string, any> | string | null,
@@ -30,6 +31,10 @@ const request = async function<T>(url: string, init: Init = { method: 'GET' }): 
         init.body = isObject(init.body) ? JSON.stringify(init.body) : init.body;
         init.mode ??= 'cors';
         init.referrerPolicy ??= 'no-referrer';
+    }
+
+    if (init.search) {
+        url = request.url(url, init.search);
     }
 
     return await fetch(url, init as RequestInit).then(r => {
