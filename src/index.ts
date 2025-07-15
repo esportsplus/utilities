661d4879d832a188b26bfbdd963c4cc7c11f76cc
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-07-14 23:04:18 -0700

    feat: add number formatting utility

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,7 @@ const isString = (value: unknown): value is string => {
 export { defineProperty, isArray, isFunction, isInstanceOf, isNumber, isObject, isString };
 export { default as bps } from './bps';
 export { default as chunk } from './chunk';
+export { default as number } from './number';
 export { default as promise } from './promise';
 export { default as request } from './request';
 export { default as sleep } from './sleep';
