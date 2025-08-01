6a0a800adaecd86b158bcab2acff496ebcea98b1
Israel Casillas Jr <ICJR@users.noreply.github.com>
2025-08-01 04:33:51 -0700

    feat: add isDate, isMap, isNumber, isPromise, isSet, isRegeExp, isSymbol

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
+let objToString: typeof Object.prototype.toString = Object.prototype.toString,
+    type = (value: unknown): string => objToString.call(value)
+
+
 const { defineProperty } = Object;
 
 const { isArray } = Array;
+
+const isDate = (val: unknown): val is Date => {
+    return type(val) === '[object Date]';
+}
 
 const isFunction = (value: unknown): value is Function => {
     return typeof value === 'function';
@@ -10,22 +18,50 @@ const isInstanceOf = <T>(instance: unknown, match: new (...args: any) => T): ins
     return typeof instance === 'object' && instance !== null && instance.constructor === match;
 };
 
+const isMap = (val: unknown): val is Map<any, any> => {
+    return type(val) === '[object Map]';
+}
+
 const isNumber = (value: any): value is number => {
     return !isNaN(value);
-};
+}
 
 const isObject = (value: unknown): value is Record<PropertyKey, unknown> => {
     return typeof value === 'object' && value !== null && value.constructor === Object;
 };
 
+const isPromise = <T = any>(val: unknown): val is Promise<T> => {
+    return (
+        (isObject(val) || isFunction(val)) &&
+        isFunction((val as any).then) &&
+        isFunction((val as any).catch)
+    )
+}
+
+const isSet = (val: unknown): val is Set<any> => {
+    return type(val) === '[object Set]';
+}
+
 const isString = (value: unknown): value is string => {
     return typeof value === 'string';
-};
+}
+
+const isRegExp = (val: unknown): val is RegExp => {
+    return type(val) === '[object RegExp]';
+}
+
+const isSymbol = (val: unknown): val is symbol => {
+    return typeof val === 'symbol';
+}
 
 const noop = (() => {}) as Readonly<VoidFunction>;
 
 
-export { defineProperty, isArray, isFunction, isInstanceOf, isNumber, isObject, isString, noop };
+export {
+    defineProperty,
+    isArray, isDate, isFunction, isInstanceOf, isMap, isNumber, isObject, isPromise, isRegExp, isSet, isString, isSymbol,
+    noop
+};
 export { default as bps } from './bps';
 export { default as chunk } from './chunk';
 export { EMPTY_ARRAY, EMPTY_OBJECT } from './constants';
